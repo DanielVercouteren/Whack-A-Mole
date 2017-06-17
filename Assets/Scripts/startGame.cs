cab848eb0427d6841df7c53e281e5f9d6a9c23ff
Daniel Vercouteren <Daniel@mbp-van-daniel.home>
2017-06-17 22:38:42 +0200

    Added singleplayer layout + scripts working

---
+++ Assets/Scripts/startGame.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class startGame : MonoBehaviour {
+	public int players;
+
+	public void switchCanvas(int players){
+		Application.LoadLevel (players);
+	}
+		
+}
